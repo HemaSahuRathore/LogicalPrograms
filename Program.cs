7126dd67f6526ca8be8bb489305ceb2ff757fa29
Hema <hemasahu019@gmail.com>
2022-07-25 17:08:26 +0530

    [Hema Added Welcome page to Main]

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace D6LogicalProblems
+{ /// <summary>
+  /// Added Welcome page
+  /// </summary>
+    public class Program
+    {
+        //An entrypoint of any console app
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Logical Programs");
+            Console.ReadLine();
+        }
+    }
+}
