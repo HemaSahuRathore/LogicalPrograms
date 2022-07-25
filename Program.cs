050d75ab028b61ab3c2e54344af197d04a2aec06
Hema <hemasahu019@gmail.com>
2022-07-25 23:35:38 +0530

    [Hema Added P12 Added code for finding Square root]

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@
             //loop to exit when use enter select exit
             do { 
                 Console.WriteLine("\nPlease select an option to procced :");
-                Console.WriteLine("1.Print Fibonacci Series \n2.Perfect Number \n3.Prime Number \n4.Reverse Number \n5.Coupon Number \n6.Stop Watch \n7.Notes Vending machine \n8.Day Of Week\n9.Temperature Conversion \n10.Monthly payment\n11.Exit  ");
+                Console.WriteLine("1.Print Fibonacci Series \n2.Perfect Number \n3.Prime Number \n4.Reverse Number \n5.Coupon Number \n6.Stop Watch \n7.Notes Vending machine \n8.Day Of Week\n9.Temperature Conversion \n10.Monthly payment\n11.Square Root \n12.Exit  ");
                 option = Convert.ToInt32(Console.ReadLine());
 
                 switch (option)
@@ -53,10 +53,13 @@
                     case 10:
                         MonthlyPayment.MonthlyPay();
                         break;
+                    case 11:
+                        Sqrt.SquareRoot();
+                        break;
 
                 }
 
-            } while(option != 11);
+            } while(option != 12);
         }
     }
 }
---
+++ SquareRoot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6LogicalProblems
+{
+    public class Sqrt
+    {
+        public static void SquareRoot()
+        {   //variables
+            double c, t, epsilon;
+
+            Console.Write("Please enter non negative number to find Square Root: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            //calculations
+            t = c;
+            epsilon = 1e-15;
+
+            while (Math.Abs(t - (c / t)) > (epsilon * t))
+            {
+                t = (c / t + t) / 2.0;
+            }
+
+            //display result
+            Console.WriteLine("Square root of {0} is {1}", c, t);
+
+        }
+    }
+}
+
