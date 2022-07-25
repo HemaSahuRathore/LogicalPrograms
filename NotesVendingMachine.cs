9427432647fa5a3b98e80ec7340471312a4c5320
Hema <hemasahu019@gmail.com>
2022-07-25 18:29:33 +0530

    [Hema Added P7 code for notes vending machine]

---
+++ NotesVendingMachine.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6LogicalProblems
+{
+    public class NotesVendingMachine
+    {   
+       public static void FindNotes()
+        {   //Array for storing Notes domination
+            int[] notes = { 1, 2, 5, 10, 50, 100, 500, 1000 };
+
+            Console.WriteLine("\nEnter the amount : ");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            int noOfNotes;
+
+            for (int i = notes.Length - 1; i > 0; i--) 
+            {
+                while (amount >= notes[i])
+                {
+                    noOfNotes = amount / notes[i]; 
+                    Console.WriteLine("\nTotal no. of {0} is {1}", notes[i], noOfNotes);
+                    amount = amount - (noOfNotes * notes[i]);
+
+                }
+            }
+        }
+
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@
             //loop to exit when use enter select exit
             do { 
                 Console.WriteLine("\nPlease select an option to procced :");
-                Console.WriteLine("1.Print Fibonacci Series \n2.Perfect Number \n3.Prime Number \n4.Reverse Number \n5.Coupon Number \n6.Stop Watch \n7.Exit  ");
+                Console.WriteLine("1.Print Fibonacci Series \n2.Perfect Number \n3.Prime Number \n4.Reverse Number \n5.Coupon Number \n6.Stop Watch \n7 Notes Vending machine \n8.Exit  ");
                 option = Convert.ToInt32(Console.ReadLine());
 
                 switch (option)
@@ -41,11 +41,14 @@
                         StopWatchSimulator stopWatch = new StopWatchSimulator();
                         stopWatch.StopWatchSimulatorP();
                         break;
+                    case 7:
+                        NotesVendingMachine.FindNotes(); //static method, calling using class name
+                        break;
 
 
                 }
 
-            } while(option != 7);
+            } while(option != 8);
         }
     }
 }
