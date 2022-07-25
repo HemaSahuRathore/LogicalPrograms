2168534b0e95fb8e15dffbea89ca138bda5eb7f0
Hema <hemasahu019@gmail.com>
2022-07-25 17:19:45 +0530

    [Hema Added P1 code to Print Fibonacci Series]

---
+++ FibonacciSeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D6LogicalProblems
+{
+    internal class Fibonacci
+    {
+        public void PrintFibonacciSeries()
+        {
+            int n, num3;
+            int num1 = 0, num2 = 1;
+
+            Console.Write("Enter the number of elements to be printer in Fibonacci Series : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write(num1 + " " + num2 + " ");
+
+            for (int i = 0; i < n; i++)
+            {
+                num3 = num1 + num2;
+                Console.Write(num3 + " ");
+                num1 = num2;
+                num2 = num3;
+
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,32 @@
 ﻿namespace D6LogicalProblems
-{ /// <summary>
-  /// Added Welcome page
-  /// </summary>
+{   //Program 1: Fibonacci Series
     public class Program
     {
         //An entrypoint of any console app
         static void Main(string[] args)
         {
+            int option; //variable declaration
+
             Console.WriteLine("Welcome to the Logical Programs");
-            Console.ReadLine();
+
+            //loop to exit when use enter select exit
+            do { 
+                Console.WriteLine("\nPlease select an option to procced :");
+                Console.WriteLine("1.Print Fibonacci Series \n2.Exit  ");
+                option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        Fibonacci fibonacci = new Fibonacci();
+                        fibonacci.PrintFibonacciSeries();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid option");
+                        break;
+                }
+
+            } while(option != 2);
         }
     }
 }
